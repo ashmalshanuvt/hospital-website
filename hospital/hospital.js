9730e35cfe98804eddc17f724fc9526c947334cc
ashmalshanuvt <167845588+ashmalshanuvt@users.noreply.github.com>
2024-04-23 12:24:05 +0530

    Add files via upload

---
+++ hospital/hospital.js
@@ -0,0 +1,7 @@
+let barsBarsButton=document.querySelector('.bars')
+let phonViewBox=document.querySelector('.phon-view-box')
+// console.log(barsBarsButton);
+
+barsBarsButton.addEventListener('click',()=>{
+   phonViewBox.classList.toggle('toggle-phon-view-box')
+})
